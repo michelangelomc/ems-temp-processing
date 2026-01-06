73f92a5510d76f72799f233f59b9c0d62b405051
michelangelomc <mickelchaves@hotmail.com>
2026-01-06 18:59:01 -0300

    Iniciando v

---
+++ src/test/java/com/yctus/sensors/temperature_processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yctus.sensors.temperature_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
