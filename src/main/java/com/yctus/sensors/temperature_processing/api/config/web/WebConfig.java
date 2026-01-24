f535ea2fdae483c27e70c96d1a30241d5f0388aa
michelangelomc <mickelchaves@hotmail.com>
2026-01-24 19:25:51 -0300

    requests - alerts - v1

--- src/main/java/com/yctus/sensors/temperature_processing/api/config/web/WebConfig.java
+++ src/main/java/com/yctus/sensors/temperature_processing/api/config/web/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration("temperatureProcessingWebConfig")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
